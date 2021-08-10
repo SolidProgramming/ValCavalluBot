cf6af9b42d4e92e15c4b241e30c5d1e38d67ed69
hbpclient <hbpclient@gmx.de>
2021-08-10 22:24:25 +0200

    .

--- Shares/Model/HorseStatsModel.cs
+++ Shares/Model/HorseStatsModel.cs
@@ -9,44 +9,17 @@ namespace Shares.Model
 {
     public class HorseStatsModel<T>
     {
-        //public event Action<T> OnHorseStatsChanged;
-
-        private T _Energy;
-        private T _Health;
-        private T _Moral;
-
-        public T Energy {
-            get 
-            {
-                return _Energy;
-            }
-            set
-            {
-                _Energy = value;
-                //OnHorseStatsChanged(value);                
-            }
+        public T Energy
+        {
+            get; set;
         }
-        public T Health {
-            get
-            {
-                return _Health;
-            }
-            set 
-            {
-                _Health = value;
-                //OnHorseStatsChanged(value);
-            }
+        public T Health
+        {
+            get; set;
         }
-        public T Moral {
-            get
-            {
-                return _Moral;
-            }
-            set
-            {
-                _Moral = value;
-               // OnHorseStatsChanged(value);
-            }
+        public T Moral
+        {
+            get;set;
         }
     }
 }
