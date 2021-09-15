7c8481e04baf03edf78c2b403e8413ce7ffd819a
luca.weidmann@gmx.de <luca.weidmann@gmx.de>
2021-09-15 13:14:59 +0200

    added howrseusermodel class added HowrseUser class

---
+++ HowrseBotClient/Class/HowrseUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HowrseBotClient.Class
+{
+    public static class HowrseUser
+    {
+        public static void GetUserInfo()
+        {
+
+        }
+    }
+}
---
+++ Shares/Model/HowrseUserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shares.Model
+{
+    public class HowrseUserModel
+    {
+        public int Eq { get; set; }
+        public int Passes { get; set; }
+        public int Rank { get; set; }
+        public int HorseCount { get; set; }
+    }
+}
